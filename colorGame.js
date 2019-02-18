3233e2f656bc6681dc248ed1f900275c76442b08
d5nguyen <d5nguyen@gmail.com>
2019-02-17 23:21:30 -0500

    Initial commit

---
+++ colorGame.js
@@ -0,0 +1,34 @@
+var colors = [
+    "rgb(255, 0, 0)",
+    "rgb(255, 255, 0)",
+    "rgb(0, 255, 0)",
+    "rgb(0, 255, 255)",
+    "rgb(0, 0, 255)",
+    "rgb(255, 0, 255)"
+]
+
+var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+var colorDisplay = document.getElementById("colorDisplay");
+
+
+colorDisplay.textContent = pickedColor;
+
+
+for(var i = 0; i < squares.length; i++){
+    //add initial colors to squares
+    squares[i].style.backgroundColor = colors[i]
+    
+
+    //add click listeners to squares
+    squares[i].addEventListener("click", function(){
+        //grab color of picked square
+        //compare color to pickedColor
+       var clickedColor = this.style.backgroundColor;
+        if(clickedColor === pickedColor) {
+            alert("Correct!")
+        } else {
+            alert("Wrong!");
+        }
+    });
+}
